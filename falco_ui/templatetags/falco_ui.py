db33427d634f8a8f7cb31cf3e0d792442197b772
Tobi DEGNON <tobidegnon@proton.me>
2024-07-22 11:46:14 +0100

    refactor: apply locality of behavior

--- falco_ui/templatetags/falco_ui.py
+++ falco_ui/templatetags/falco_ui.py
@@ -1,22 +1,8 @@
 from __future__ import annotations
 
 from django import template
-from django.templatetags.static import static
-from django.utils.html import format_html
 
 register = template.Library()
-
-
-@register.simple_tag
-def falco_ui_script(
-    alpinejs: str = "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js",
-) -> str:
-    return format_html(
-        '<script src="{}" defer></script>\n<script src="{}"></script>',
-        alpinejs,
-        static("falco-ui/main.js"),
-    )
-
 
 try:
     from heroicons.templatetags.heroicons import (
