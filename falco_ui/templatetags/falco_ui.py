f97570607a2a9a255f176b3d807ffda879e7c6bc
Tobi DEGNON <tobidegnon@proton.me>
2024-07-23 09:36:34 +0100

    classic component naming style

--- falco_ui/templatetags/falco_ui.py
+++ falco_ui/templatetags/falco_ui.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from django import template
+from typing import Callable
 
 register = template.Library()
 
@@ -17,15 +18,14 @@ else:
 
     @register.simple_tag
     def heroicon_wrapper(style: str, attrs: str | None = None):
-        style_tag_map: dict[str, callable] = {
+        style_tag_map: dict[str, Callable] = {
             "outline": heroicon_outline,
             "solid": heroicon_solid,
             "mini": heroicon_mini,
             "micro": heroicon_micro,
         }
-        if attrs is None:
-            attrs = {}
-        else:
-            attrs = dict([attr.split("=") for attr in attrs.split()])
-            attrs = {k: v.strip('"') for k, v in attrs.items()}
-        return style_tag_map[style](**attrs)
+        parsed_attrs = {}
+        if attrs:
+            parsed_attrs = dict([attr.split("=") for attr in attrs.split()])
+            parsed_attrs = {k: v.strip('"') for k, v in parsed_attrs.items()}
+        return style_tag_map[style](**parsed_attrs)
